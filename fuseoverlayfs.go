8521793a789089cf232b1732a69f87175bd5d53e
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2020-11-05 17:04:59 +0900

    Port over Config.RootPath from overlay plugin
    From https://github.com/containerd/containerd/commit/5ed177a2da0e1eb41ebd5cee63616fb7a780e0ac
    
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- fuseoverlayfs.go
+++ fuseoverlayfs.go
@@ -43,12 +43,24 @@ const (
 
 func init() {
 	plugin.Register(&plugin.Registration{
-		Type: plugin.SnapshotPlugin,
-		ID:   "fuse-overlayfs",
+		Type:   plugin.SnapshotPlugin,
+		ID:     "fuse-overlayfs",
+		Config: &Config{},
 		InitFn: func(ic *plugin.InitContext) (interface{}, error) {
 			ic.Meta.Platforms = append(ic.Meta.Platforms, platforms.DefaultSpec())
-			ic.Meta.Exports["root"] = ic.Root
-			return NewSnapshotter(ic.Root)
+
+			config, ok := ic.Config.(*Config)
+			if !ok {
+				return nil, errors.New("invalid fuse-overlayfs configuration")
+			}
+
+			root := ic.Root
+			if config.RootPath != "" {
+				root = config.RootPath
+			}
+
+			ic.Meta.Exports["root"] = root
+			return NewSnapshotter(root)
 		},
 	})
 }
